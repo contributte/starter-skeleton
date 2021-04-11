8d9612ed1566a8f5faf282c94fd18e962ae1ea64
Oh My Felix <ohmyfelixbot@gmail.com>
2021-04-11 17:10:23 +0200

    Router: fix return type

--- app/model/RouterFactory.php
+++ app/model/RouterFactory.php
@@ -2,14 +2,14 @@
 
 namespace App\Model;
 
-use Nette\Application\IRouter;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
+use Nette\Routing\Router;
 
 class RouterFactory
 {
 
-	public static function createRouter(): IRouter
+	public static function createRouter(): Router
 	{
 		$router = new RouteList();
 		$router[] = new Route('<presenter>/<action>', 'Home:default');
